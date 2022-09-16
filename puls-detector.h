b659dc6f199dc1405c8cfcf4ded746611c502bb2
a-mikhaylov <lexarobotr2d2@gmail.com>
2022-09-16 15:41:57 +0300

    v1 - non_tested

--- puls-detector.h
+++ puls-detector.h
@@ -8,8 +8,18 @@ using namespace std;
 // #define INF 999999
 
 struct puls {
-    int32_t index;
-    int32_t A;
+    int32_t     iup;
+    int32_t     imx;
+    int32_t     idn;
+    int32_t     iPrevMin;
+
+
+    puls() {
+        iup         = 0;
+        imx         = 0;
+        idn         = 0;
+        iPrevMin    = 0;
+    }
 };
 
 class PulsDetector {
@@ -21,15 +31,12 @@ public:
     int32_t     mxCnt;
 
     bool        lookMn;
-    int32_t     mnCnt0;
-    int32_t     mnCnt1;
-    int32_t     mnVal0;
-    int32_t     mnVal1;
+    int32_t     mnCnt0, mnCnt1;
+    int32_t     mnVal0, mnVal1;
     int32_t     mnPrevPos;
 
     float       minR;
-    int32_t     LvZ;
-    int32_t     LvR;
+    int32_t     LvZ, LvR;
     int32_t     kR;
 
     float       kZ;
@@ -38,11 +45,36 @@ public:
     int32_t     dn, pkDn;
     bool        lookDn;
 
-	PulsDetector() {
-		
+    //struct pk1
+    puls        pk;
+
+	PulsDetector(int32_t Fs, int32_t MinDistance) {
+		pk          = puls();
+        minDist     = MinDistance;
+        LvP         = INT32_MIN;
+        mxCnt       = 0;
+
+        lookMn      = false;
+        mnCnt0      = 1;
+        mnCnt1      = 1;
+        mnPrevPos   = 0;
+
+        minR        = 0.1; //*mmlsb
+        LvZ         = 1;   //*mmlsb
+        kR          = 1/400*(1000/Fs);
+
+        kZ          = 0.2;
+        up          = 1;
+        dn          = 1;
+        modeZero    = true;
+        pkUp        = 1;
+        pkDn        = 1;
+        lookDn      = false;
 	}
 
     bool exe(int tacho, int base) {
+        bool res = false;
+
 		if (mnVal0 >= base) { mnVal0 = base; mnCnt0 = 0; }
         if (mnVal1 >= base) { mnVal1 = base; mnCnt1 = 0; }
         
@@ -88,11 +120,30 @@ public:
 
         if (mxCnt == minDist) {
             if (LvP > LvZ && !lookDn) {
-                
+                pk.iup         = pkUp;
+                pk.imx         = i - mxCnt;
+                pk.idn         = pkDn;
+                pk.iPrevMin    = mnPrevPos;
+
+                mnCnt1 = mnCnt0;
+                mnVal1 = mnVal0;
+            
+                lookMn  = true;
+                res     = true;
             }
+
+            int32_t dZ;
+            int32_t err = LvP/2 - LvZ;
+            if (err >  LvZ) { err =  LvZ; }
+            if (err < -LvZ) { err = -LvZ; }
+            if (err > 0)    { dZ  = err * kZ; }
+            else            { dZ  = err * kZ * 4; }
+
+            LvZ = LvZ + dZ;
+            LvP = 0;
         }
 
         i++;
-        return false;
+        return res;
 	}
 };
